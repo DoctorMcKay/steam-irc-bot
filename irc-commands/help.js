5969bfc88e3186c4b00e3c0b179a74f4ce941830
Alexander Corn <mckay@doctormckay.com>
2015-09-10 22:58:06 -0400

    Make help command less terrible

--- irc-commands/help.js
+++ irc-commands/help.js
@@ -3,24 +3,30 @@ module.exports = function(prefixMsg) {
 		"triggers": ["help"],
 		"description": "Get list of available IRC commands",
 		"callback": function(sender, args) {
-			var lines = [];
-			var i, j;
 
 			var commands = require.main.exports.ircCommands;
 			
+			if(args.length > 0 && commands[args[0].toLowerCase()]) {
+				var command = commands[args[0].toLowerCase()];
+				
+				require.main.exports.irc.notice(sender.nick, command.triggers[0] + (command.triggers.length > 1 ? " (" + command.triggers.slice(1).join(', ') + ")" : '') + " - " + command.description);
+				return;
+			}
+			
+			var list = [];
+			var i, j;
 			for(i in commands) {
 				if(i != commands[i].triggers[0]) {
 					// Alias
 					continue;
 				}
-
-				lines.push(i + (commands[i].triggers.length > 1 ? " (" + commands[i].triggers.slice(1).join(', ') + ") " : '') + " - " + commands[i].description);
+				
+				list.push(i);
 			}
 			
-			lines.sort();
-			for(i = 0; i < lines.length; i++) {
-				require.main.exports.irc.notice(sender.nick, lines[i]);
-			}
+			list.sort();
+			
+			require.main.exports.irc.notice(sender.nick, "Commands: " + list.join(', '));
 		}
 	};
 };
